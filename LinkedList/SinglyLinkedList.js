1c46ff7cddb586f60b688fb1553ed5e0848f39a3
loseway216 <loseway216@gmail.com>
2023-08-19 23:20:12 +0800

    test: add unit test

--- LinkedList/SinglyLinkedList.js
+++ LinkedList/SinglyLinkedList.js
@@ -5,7 +5,7 @@ class Node {
   }
 }
 
-module.exports = class LinkedList {
+class LinkedList {
   constructor() {
     this.head = null;
   }
@@ -146,51 +146,6 @@ module.exports = class LinkedList {
       return st;
     }
   }
-};
+}
 
-// test insertAtTail
-// let list = new LinkedList();
-// for (var i = 0; i < 5; i++) {
-//   list.insertAtTail(i);
-//   list.printList();
-// }
-
-// test search
-// let list = new LinkedList();
-// list.insertAtHead(4);
-// list.insertAtHead(10);
-// list.insertAtHead(40);
-// list.insertAtHead(5);
-// list.printList();
-// console.log(list.search(4));
-// console.log(list.search(0));
-
-// test deleteVal
-// let list = new LinkedList();
-// list.insertAtHead(2);
-// list.insertAtHead(4);
-// list.insertAtHead(5);
-// list.insertAtHead(7);
-// list.insertAtHead(1);
-// list.printList();
-// list.deleteVal(2);
-// list.printList();
-
-// test deleteAtTail
-// let list = new LinkedList();
-// list.insertAtHead(2);
-// list.insertAtHead(4);
-// list.insertAtHead(5);
-// list.insertAtHead(7);
-// list.insertAtHead(1);
-// list.printList();
-// list.deleteAtTail();
-// list.printList();
-// list.deleteAtTail();
-// list.printList();
-// list.deleteAtTail();
-// list.printList();
-// list.deleteAtTail();
-// list.printList();
-// list.deleteAtTail();
-// list.printList();
+export default LinkedList;
---
+++ __test__/SinglyLinkedList.test.js
@@ -0,0 +1,53 @@
+import LinkedList from "../LinkedList/SinglyLinkedList";
+
+describe("SinglyLinkedList", () => {
+  test("insertAtTail", () => {
+    const link = new LinkedList();
+  });
+});
+// test insertAtTail
+// let list = new LinkedList();
+// for (var i = 0; i < 5; i++) {
+//   list.insertAtTail(i);
+//   list.printList();
+// }
+
+// test search
+// let list = new LinkedList();
+// list.insertAtHead(4);
+// list.insertAtHead(10);
+// list.insertAtHead(40);
+// list.insertAtHead(5);
+// list.printList();
+// console.log(list.search(4));
+// console.log(list.search(0));
+
+// test deleteVal
+// let list = new LinkedList();
+// list.insertAtHead(2);
+// list.insertAtHead(4);
+// list.insertAtHead(5);
+// list.insertAtHead(7);
+// list.insertAtHead(1);
+// list.printList();
+// list.deleteVal(2);
+// list.printList();
+
+// test deleteAtTail
+// let list = new LinkedList();
+// list.insertAtHead(2);
+// list.insertAtHead(4);
+// list.insertAtHead(5);
+// list.insertAtHead(7);
+// list.insertAtHead(1);
+// list.printList();
+// list.deleteAtTail();
+// list.printList();
+// list.deleteAtTail();
+// list.printList();
+// list.deleteAtTail();
+// list.printList();
+// list.deleteAtTail();
+// list.printList();
+// list.deleteAtTail();
+// list.printList();
